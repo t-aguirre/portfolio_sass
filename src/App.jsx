a528025bca9e866e9338c6f42a69c42987ee4024
t-aguirre <100530379+t-aguirre@users.noreply.github.com>
2022-04-24 16:34:49 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <p className="app__text">
+        Ce template est bien configuré pour vos applications front-end avec
+        Vite, Sass et Vercel
+      </p>
+    </div>
+  );
+}
+
+export default App;
